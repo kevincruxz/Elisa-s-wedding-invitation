352041b8360882ee481bdbd571f522c1fa596a0a
Kevin Cruz <kevin_cruzz@outlook.es>
2022-08-06 22:43:47 -0500

    Added countdown

---
+++ script.js
@@ -0,0 +1,19 @@
+const date = new Date("Apr 8, 2023 17:00:00").getTime();
+
+let setCountdown = setInterval(function() {
+    let currentDate = new Date().getTime();
+    let timeLeft = date - currentDate;
+
+    let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    displayCountdown(days, hours, minutes, seconds);
+}, 1000);
+
+function displayCountdown(days, hours, minutes, seconds) {
+    document.getElementById('days').innerHTML = days + " Dias :";
+    document.getElementById('hours').innerHTML = hours + " Horas :";
+    document.getElementById('minutes').innerHTML = minutes + " Minutos :";
+    document.getElementById('seconds').innerHTML = seconds + " Segundos";
+}
