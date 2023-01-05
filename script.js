d263c1d5a3d0f9f9753cd4d92207d3f4e8e45406
Kevin Cruz <kevin_cruzz@outlook.es>
2023-01-05 00:32:28 -0600

    Finished script for section 3

--- script.js
+++ script.js
@@ -38,63 +38,79 @@ let holaAnimation = setInterval(function() {
 }, 300);
 
 let botones = document.querySelectorAll('.botones');
-let detailsOpen = false;
 
 botones.forEach(function(boton) {
     boton.addEventListener('click', (e) => {
-        let descripcion = document.querySelector(getTheDiv(boton.id));
-        if (detailsOpen) {
+        let allData = getEverything(boton.id);
+        let descripcion = document.querySelector(allData[0]);
+
+        if (descripcion.classList.contains('agrandar-descripcion')) {
+            descripcion.style.cssText = '';
             descripcion.classList.remove('agrandar-descripcion');
             descripcion.classList.remove('animate__animated');
-            descripcion.classList.remove('animate__flip');
-            detailsOpen = false;
-        } else {
+            descripcion.classList.remove('animate__jackInTheBox');
+            
+            descripcion.innerHTML = allData[2];
             descripcion.classList.add('animate__animated');
-            descripcion.classList.add('animate__flip');
+            descripcion.classList.add('animate__bounceInDown');
+        } else {
+
+            descripcion.classList.remove('animate__animated');
+            descripcion.classList.remove('animate__bounceInDown');
+
+            descripcion.style.cssText = `background: url(img/${allData[3]}); background-size: cover; background-position: center; background-repeat: no-repeat;`;
+            
+            descripcion.classList.add('animate__animated');
+            descripcion.classList.add('animate__jackInTheBox');
             descripcion.classList.add('agrandar-descripcion');
-            descripcion.innerHTML = "asdad asd asdsad adasd asdasdasdad asdasdasd";
-            detailsOpen = true;
+            descripcion.innerHTML = allData[1];
         }   
     });
 });
 
-function getTheDiv(id) {
+function getEverything(id) {
+    arr = []; //arr[0] = id, arr[1] = description, arr[2] = default, arr[3] = background
+
     if (id === 'cuando') {
-        return "#descripcion";
+        arr[0] = "#descripcion";
+        arr[1] = "Cuando llegues al parque Chipinque menciona que asistiras a la boda de Elisa y Pedro. Conduce hasta llegar al hotel";
+        arr[2] = "Llegada a chipinque";
+        arr[3] = "chipinque.jpg";
     } else if (id === 'cuando-dos') {
-        return "#descripcion-dos";
+        arr[0] = "#descripcion-dos";
+        arr[1] = "Realiza tu check in en el hotel";
+        arr[2] = "Recepcion en hotel chipinque";
+        arr[3] = "hotel.jpg";
     } else if (id === 'cuando-tres') {
-        return "#descripcion-tres";
+        arr[0] = "#descripcion-tres";
+        arr[1] = "Se celebrará en una capilla al aire libre dentro del hotel Chipinque";
+        arr[2] = "Ceremonia religiosa";
+        arr[3] = "misa.png";
     } else if (id === 'cuando-cuatro') {
-        return "#descripcion-cuatro";
+        arr[0] = "#descripcion-cuatro";
+        arr[1] = "Se realizará justo despues de la ceremonia religiosa en un mirador con vistas a toda la ciudad de Monterrey";
+        arr[2] = "Brindis";
+        arr[3] = "brindis.jpg";
     } else if (id === 'cuando-cinco') {
-        return "#descripcion-cinco";
+        arr[0] = "#descripcion-cinco";
+        arr[1] = "Podrás consumir los dulces que se encontrarán en mesas alrededor del mirador";
+        arr[2] = "Mesa de dulces";
+        arr[3] = "mesadedulces.jpeg";
     } else if (id === 'cuando-seis') {
-        return "#descripcion-seis";
+        arr[0] = "#descripcion-seis";
+        arr[1] = "En la explanada del hotel se localizara un fotografo profesional para tomar fotos con o sin los novios";
+        arr[2] = "Fotografias";
+        arr[3] = "fotos.jpeg";
     } else if (id === 'cuando-siete') {
-        return "#descripcion-siete";
-    } else if (id === 'cuando-ocho') {
-        return "#descripcion-ocho";
+        arr[0] = "#descripcion-siete";
+        arr[1] = "Tendra lugar a partir de las 19:00 en una explanada al costado del mirador de Chipinque.";
+        arr[2] = "Fiesta";
+        arr[3] = "fiesta.jpg";
+    } else {
+        arr[0] = "#descripcion-ocho";
+        arr[1] = "Se servira un menu de tres tiempos conformado por platillos muy ricos.";
+        arr[2] = "Servicio de cena";
+        arr[3] = "cena.jpg";
     }
+    return arr;
 }
-
-/*
-    // detect when element gets visible on scroll
-    document.addEventListener("scroll", (e) => {
-        var element = document.getElementById("img-lisa");
-
-        var top_of_element = element.offsetTop;
-        var bottom_of_element = element.offsetTop + element.offsetHeight + element.style.marginTop;
-        var bottom_of_screen = window.scrollY + window.innerHeight;
-        var top_of_screen = window.scrollY;
-
-        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
-            // Element is visible write your codes here
-            // You can add animation or other codes here
-            element.classList.add('img-on-screen');
-        } else {
-            // the element is not visible, do something else
-            element.classList.remove('img-on-screen');
-        }
-    });
-*/
